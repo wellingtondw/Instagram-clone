e43b4907f69b5f9404bcf36313af92bf4d894a3c
Wellington Lima <wellingtonslpbi@hotmail.com>
2020-02-10 22:03:23 -0300

    list feed items

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,8 +1,76 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, FlatList } from "react-native";
 
-// import { Container } from './styles';
+import {
+  Post,
+  Header,
+  Avatar,
+  Name,
+  PostImage,
+  Description,
+  Loading
+} from "./styles";
 
 export default function Feed() {
-  return <View />;
+  const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  async function loadPage(pageNumber = page, shouldRefresh = false) {
+    if (total && page > total) return;
+
+    setLoading(true);
+
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`
+    );
+
+    const data = await response.json();
+    const totalItems = response.headers.get("X-Total-Count");
+
+    setTotal(Math.floor(totalItems / 5));
+    setFeed(shouldRefresh ? data : [...feed, ...data]);
+    setPage(pageNumber + 1);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadPage();
+  }, []);
+
+  async function refreshList() {
+    setRefreshing(true);
+
+    await loadPage(1, true);
+
+    setRefreshing(false);
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={feed}
+        keyExtractor={post => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
+        onRefresh={refreshList}
+        refreshing={refreshing}
+        ListFooterComponent={loading && <Loading />}
+        renderItem={({ item }) => (
+          <Post>
+            <Header>
+              <Avatar source={{ uri: item.author.avatar }} />
+              <Name>{item.author.name}</Name>
+            </Header>
+            <PostImage ratio={item.aspectRatio} source={{ uri: item.image }} />
+            <Description>
+              <Name>{item.author.name}</Name> {item.description}
+            </Description>
+          </Post>
+        )}
+      />
+    </View>
+  );
 }
